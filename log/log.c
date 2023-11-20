6e26d5646c207e9eb994fdd758109aaa545717c8
Adisorn Parama <61809305+DarkTouiZ@users.noreply.github.com>
2023-11-20 12:48:54 +0700

    Create log.c
    contain conversation of user and chat in the log files

---
+++ log/log.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main()
+
+{
+    //create a file
+    FILE *fp;
+    char nameLog[10000];
+    scanf(" %s", nameLog);
+    //open file in log folder
+    fp = fopen(nameLog, "w");
+
+    //include date and time
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    fprintf(fp, "Date: %d-%d-%d   ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+    fprintf(fp, "Time: %d:%d\n", tm.tm_hour, tm.tm_min);
+    
+    //question until "EXIT"
+    char input[10000];
+    char respones[10000];
+    do{
+        //receive user input
+        //input include " "
+        scanf(" %[^\n]s", input);
+        //if input is "EXIT" upper case or lower case
+        if(strcmp(input, "EXIT") == 0 || strcmp(input, "exit") == 0){
+            break;
+        }
+        fprintf(fp, "User: %s\n", input);
+            
+        //respones include " "
+        scanf(" %[^\n]s", respones);
+        fprintf(fp, "Chat: %s\n", respones);
+    }while(1);
+    fclose(fp);
+
+    return 0;
+}
